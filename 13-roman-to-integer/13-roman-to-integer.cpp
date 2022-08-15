40716d9b8bca1054aa5205e96ba4cfa307411852
keshavagarwal2580 <71182804+keshavagarwal2580@users.noreply.github.com>
2022-08-15 21:46:28 +0530

    Time: 15 ms (62.96%), Space: 7.7 MB (49.47%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -4,8 +4,8 @@ public:
         unordered_map<char,int> mp={{'I',1},{'V',5},{'X',10},{'L',50},{'C',100},{'D',500},{'M',1000}};
         
         int sum=mp[s.back()];
-        for(int i=s.size()-2;i>=0;i--){
-            if(mp[s[i]]<mp[s[i+1]])
+        for(int i=0;i<s.size()-1;i++){
+            if(mp[s[i+1]]>mp[s[i]])
                 sum-=mp[s[i]];
             else
                 sum+=mp[s[i]];
