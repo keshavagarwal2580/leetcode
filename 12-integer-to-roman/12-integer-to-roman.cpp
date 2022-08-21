274055e09e9f47223072f083dbf577da09a2980a
keshavagarwal2580 <71182804+keshavagarwal2580@users.noreply.github.com>
2022-08-21 17:34:33 +0530

    Time: 3 ms (96.74%), Space: 6 MB (49.71%) - LeetHub

--- 12-integer-to-roman/12-integer-to-roman.cpp
+++ 12-integer-to-roman/12-integer-to-roman.cpp
@@ -6,7 +6,7 @@ public:
         int arr[13]={1000,900,500,400,100,90,50,40,10,9,5,4,1};
         string rom[13]={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
         
-        for(int i=0;n!=0;i++){
+        for(int i=0;i<13;i++){
             while(n>=arr[i]){
                 n=n-arr[i];
         s+=rom[i];
