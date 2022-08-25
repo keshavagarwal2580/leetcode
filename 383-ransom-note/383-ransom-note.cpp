f5e34b58b7b44d13ba59ae54e85e9aa1d021688a
keshavagarwal2580 <71182804+keshavagarwal2580@users.noreply.github.com>
2022-08-25 22:24:32 +0530

    Time: 37 ms (31.12%), Space: 8.8 MB (39.49%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -1,22 +1,21 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        unordered_map<char,int>m;
-        for(auto it: magazine){
-            m[it]++;
+        unordered_map<char,int> m;
+        for(int i=0;i<magazine.size();i++){
+            m[magazine[i]]++;
         }
         
         for(int i=0;i<ransomNote.size();i++){
             if(m.find(ransomNote[i])!=m.end()){
                 if(m[ransomNote[i]]<1){
-                    return false;
+                return false;
                 }
-                else m[ransomNote[i]]--;
+                else
+                    m[ransomNote[i]]--;
             }
             else return false;
-            
         }
-            return true;
-        
+        return true;
     }
 };
