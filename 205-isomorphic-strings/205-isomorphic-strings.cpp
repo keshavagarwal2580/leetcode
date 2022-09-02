c0e799d6f08c9857e3a721b6bebf48f93f34b53f
keshavagarwal2580 <71182804+keshavagarwal2580@users.noreply.github.com>
2022-09-02 11:59:47 +0530

    Time: 39 ms (5.04%), Space: 7.2 MB (20.29%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        unordered_map<char,char> m,m4;
-        unordered_map<char,bool> m2,m3;
         if(s.size()!=t.size()) return false;
+        map<char,char> a,b;
         for(int i=0;i<s.size();i++){
-            if(m2[s[i]]!=1 && m3[t[i]]!=1){
-                m[s[i]]=t[i];
-                m4[t[i]]=s[i];
-                m2[s[i]]=1;
-                m3[t[i]]=1;
+            if(a.find(s[i])!=a.end()){
+                if(a[s[i]]!=t[i]) return false;
             }
-                  else
-                  if(m[s[i]]!=t[i] || m4[t[i]]!=s[i]) return false;
+            if(b.find(t[i])!=b.end()){
+                if(b[t[i]]!=s[i]) return false;
+            }
+            a[s[i]]=t[i];
+            b[t[i]]=s[i];
         }
-                  return true;
+        return true;
     }
 };
