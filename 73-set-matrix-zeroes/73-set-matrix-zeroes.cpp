7ea64cdd9ab905c3308baf1e9251b7e54068a20d
keshavagarwal2580 <71182804+keshavagarwal2580@users.noreply.github.com>
2022-10-09 15:11:46 +0530

    Time: 45 ms (5.51%), Space: 13.3 MB (20.28%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,36 +3,35 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         
         bool row=false,col=false;
-        
         for(int i=0;i<matrix.size();i++){
-            if(matrix[i][0]==0)
-                row=true;
-        
+            if(matrix[i][0]==0) row=true;
+        }
         
         for(int j=0;j<matrix[0].size();j++){
-            if(matrix[0][j]==0)
-                col=true;
+            if(matrix[0][j]==0) col=true;
         }
-        }
+        
+        
         for(int i=1;i<matrix.size();i++){
             for(int j=1;j<matrix[0].size();j++){
-                if(matrix[i][j]==0){
-                    matrix[0][j]=0;
+                if(matrix[i][j]==0)
+                {
                     matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
         
         for(int i=1;i<matrix.size();i++){
             for(int j=1;j<matrix[0].size();j++){
-                if(matrix[0][j]==0 || matrix[i][0]==0){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
             }
         }
         
-        if(row)
-        {
+        
+        if(row){
             for(int i=0;i<matrix.size();i++){
                 matrix[i][0]=0;
             }
@@ -43,5 +42,7 @@ public:
                 matrix[0][j]=0;
             }
         }
+        
+        
     }
 };
